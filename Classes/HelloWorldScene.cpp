bfeb47fd37d073bf23238cc2647e5207f5138226
tiger <tahmidsadik112@gmail.com>
2015-05-02 06:43:39 +0600

    Added Proper shooting mechanics

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -7,6 +7,7 @@ bool moveRight;
 bool moveUp;
 bool moveDown;
 bool isShooting;
+bool visibility;
 cocos2d::Sprite *sprite;
 
 void initDefaultMove() {
@@ -15,6 +16,7 @@ void initDefaultMove() {
     moveUp = false;
     moveDown = false;
     isShooting = false;
+    visibility = false;
 }
 
 Scene* HelloWorld::createScene()
@@ -72,7 +74,6 @@ bool HelloWorld::init()
     
     
     auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
     // position the label on the center of the screen
     label->setPosition(Vec2(origin.x + visibleSize.width/2,
                             origin.y + visibleSize.height - label->getContentSize().height));
@@ -82,7 +83,7 @@ bool HelloWorld::init()
 
     // add "HelloWorld" splash screen"
     sprite = Sprite::create("Spaceship.png");
-    sprite->setScale(0.2, 0.2);
+    sprite->setScale(0.75, 0.75);
     
 //    emissionParticle->setTotalParticles(77);
 //    emissionParticle->setDuration(ParticleSystem::DURATION_INFINITY);
@@ -90,21 +91,25 @@ bool HelloWorld::init()
 
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-    
-    auto emissionParticle = ParticleSystemQuad::create("particle_texture.plist");
-    emissionParticle->setPosition(Vec2(origin.x + visibleSize.width / 2,
-                                       origin.y + visibleSize.height /2 - 20));
+
+    auto emissionParticle = ParticleSystemQuad::create("FireEmission.plist");
+    auto emitParticle2 = ParticleSystemQuad::create("FireEmission.plist");
+    emissionParticle->setPosition(Vec2(sprite->getPositionX() - 260,
+                                       sprite->getPositionY() - 320));
+    emitParticle2->setPosition(Vec2(sprite->getPositionX() - 303,
+                                       sprite->getPositionY() - 320));
+    sprite->addChild(emissionParticle);
+    sprite->addChild(emitParticle2);
 
     
 
 
     // add the sprite as a child to this layer
     this->addChild(sprite, 0);
-    this->addChild(emissionParticle);
     
     //creating a keyboard event listener
     auto listener = EventListenerKeyboard::create();
-    listener->onKeyPressed = [] (EventKeyboard::KeyCode keycode, Event* event) -> void {
+    listener->onKeyPressed = [=] (EventKeyboard::KeyCode keycode, Event* event) -> void {
         switch (keycode) {
             case EventKeyboard::KeyCode::KEY_W:
                 moveUp = true;
@@ -119,13 +124,18 @@ bool HelloWorld::init()
                 moveRight = true;
                 break;
             case EventKeyboard::KeyCode::KEY_SPACE:
-                isShooting = true;
-            default:
+//                isShooting = true;
+                auto bullet = Sprite::create("bullet.png");
+                bullet->setPosition(sprite->getPosition());
+                bullet->setScale(4.0, 4.0);
+                this->addChild(bullet);
+                bullet->runAction(MoveTo::create(0.4, Vec2(sprite->getPositionX(), visibleSize.height + 100)));
+                this->schedule(schedule_selector(HelloWorld::shoot), 0.2f);
                 break;
         }
     };
     
-    listener->onKeyReleased = [] (EventKeyboard::KeyCode keycode, Event* event) -> void {
+    listener->onKeyReleased = [=] (EventKeyboard::KeyCode keycode, Event* event) -> void {
                 switch (keycode) {
                     case EventKeyboard::KeyCode::KEY_W:
                         moveUp = false;
@@ -141,6 +151,7 @@ bool HelloWorld::init()
                         break;
                     case EventKeyboard::KeyCode::KEY_SPACE:
                         isShooting = false;
+                        this->unschedule(schedule_selector(HelloWorld::shoot));
                         break;
                     default:
                         break;
@@ -153,9 +164,17 @@ bool HelloWorld::init()
     return true;
 }
 
+void HelloWorld::shoot(float dt) {
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    auto bullet = Sprite::create("bullet.png");
+    bullet->setPosition(sprite->getPosition());
+    bullet->setScale(4.0, 4.0);
+    this->addChild(bullet);
+    bullet->runAction(MoveTo::create(0.4, Vec2(sprite->getPositionX(), visibleSize.height + 100)));
+}
+
 void HelloWorld::update(float dt) {
-    auto visisbleSize = Director::getInstance()->getVisibleSize();
-    
+    bool ableToShoot = false;
     int playerSpeed = 7;
     
     if (moveLeft && moveDown) {
@@ -176,11 +195,15 @@ void HelloWorld::update(float dt) {
         sprite->runAction(MoveBy::create(dt, Vec2(0, -playerSpeed)));
     }
     
-    if (isShooting) {
-        auto bullet = Sprite::create("particle_texture.png");
+    if(isShooting) {
+        
+        ableToShoot = false;
+        auto visibleSize = Director::getInstance()->getVisibleSize();
+        auto bullet = Sprite::create("bullet.png");
         bullet->setPosition(sprite->getPosition());
+        bullet->setScale(4.0, 4.0);
         this->addChild(bullet);
-        bullet->runAction(MoveTo::create(0.4, Vec2(bullet->getPositionX(), visisbleSize.height)));
+        bullet->runAction(MoveTo::create(0.4, Vec2(sprite->getPositionX(), visibleSize.height + 100)));
     }
 }
 
